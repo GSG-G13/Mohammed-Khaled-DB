9b1dfd28581a551251bffdf88966bb46a41a1123
MohammedSallout <msallout294@gmail.com>
2023-04-05 13:05:01 +0300

    connection database Relates #10

---
+++ server/database/config/connection.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+if (!process.env.DB_URL) throw new Error('No Database URL!!!')
+
+const options = {
+  connectionString: process.env.DB_URL,
+  ssl: false
+}
+
+const connection = new Pool(options)
+
+module.exports = { connection }
